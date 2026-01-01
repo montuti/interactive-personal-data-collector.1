d22b0e669052ef2bd3bf7a76a82468b9199e7bf1
montuti <montuhardik123@gmail.com>
2026-01-01 22:35:40 +0530

    Add files via upload

---
+++ FundamentalBooster.py
@@ -0,0 +1,35 @@
+# Interactive Personal Data Collector
+# Fixed User Data Example
+
+print("Welcome to the Interactive Personal Data Collector!\n")
+
+# -------- User Data --------
+name = "Montu"
+age = 20
+height = 5.6
+fav_number = 2
+birth_year = 2005
+
+# -------- Display Variable Details --------
+print("Collected Data Details:\n")
+
+print("Name:", name)
+print("Type:", type(name))
+print("Memory Address:", id(name))
+print()
+
+print("Age:", age)
+print("Type:", type(age))
+print("Memory Address:", id(age))
+print()
+
+print("Height:", height)
+print("Type:", type(height))
+print("Memory Address:", id(height))
+print()
+
+print("Favourite Number:", fav_number)
+print("Type:", type(fav_number))
+print("Memory Address:", id(fav_number))
+
+
